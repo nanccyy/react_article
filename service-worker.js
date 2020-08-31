e6c8510e09520fb39bffae376a8093a1db1eef14
nanccyy <lidiapriymak@gmail.com>
2020-08-31 14:59:56 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.575c894ccaaaec018efa3e6d1c5de3fa.js"
+  "./precache-manifest.f2ff650f107874ae42146e637d5ed6d0.js"
 );
 
 self.addEventListener('message', (event) => {
